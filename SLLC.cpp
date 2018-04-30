d18a38f9bf6c330793744099df4a6765f02a940a
ArifPebriansyah1014 <38611082+ArifPebriansyah1014@users.noreply.github.com>
2018-04-30 23:20:47 +0700

    Update SLLC.cpp

--- SLLC.cpp
+++ SLLC.cpp
@@ -128,6 +128,43 @@ void single_list::insert_last(){
 //case 3
 
 
+//case 5
+void single_list::insert_pos(){
+	int value, pos, counter = 0;
+	cout<<"masukan value: ";cin>>value;
+	struct node *temp, *s, *ptr;
+	temp = create_node(value);
+	cout<<"masukan posisi yang ingin dituju";cin>>pos;
+	int i;
+	s = start;
+	while (s!=NULL){
+		s = s->next;
+		counter ++;
+	}
+	if (pos==1){
+		if (start == NULL){
+		start = temp;
+		start->next= NULL;
+		} else {
+			ptr = start;
+			start = temp;
+			start->next=ptr;
+		}
+	}
+	else if (pos>1 && pos <= counter){
+			s = start;
+			for (i=1; i<pos;i++){
+				ptr =s;
+				s=s->next;
+			}
+			ptr->next = temp;
+			temp->next = s;
+		} else {
+			cout<<"posisi terlalu jauh"<<endl;	
+		}	
+}
+
+
 
 
 
